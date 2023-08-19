c3f9ac78da6d69c4bc0ee29cb3980f4db36ed466
xiangkexin <617904454@qq.com>
2023-08-19 21:06:19 +0800

    岛问题

---
+++ src/chapter_7_bitoperation/Problem_07_Islands.java
@@ -0,0 +1,50 @@
+package chapter_7_bitoperation;
+
+public class Problem_07_Islands {
+
+	public static int countIslands(int[][] m) {
+		if (m == null || m[0] == null) {
+			return 0;
+		}
+
+		int count = 0;
+		for (int i = 0; i < m.length; i++) {
+			for (int j = 0; j < m[0].length; j++) {
+				if (m[i][j] == 1) {
+					count++;
+					infect(m, i, j);
+				}
+			}
+		}
+		return count;
+	}
+
+	// 假设m矩阵的大小为N行M列，从i行j列开始感染过程
+	public static void infect(int[][] m, int i, int j) {
+		if (i < 0 || i > m.length - 1 || j < 0 || j > m[0].length - 1
+				|| m[i][j] != 1) {
+			return;
+		}
+		m[i][j] = 2;
+		infect(m, i + 1, j);
+		infect(m, i - 1, j);
+		infect(m, i, j + 1);
+		infect(m, i, j - 1);
+	}
+
+	public static void main(String[] args) {
+		int[][] m1 = { { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+				{ 0, 1, 1, 1, 0, 1, 1, 1, 0 }, { 0, 1, 1, 1, 0, 0, 0, 1, 0 },
+				{ 0, 1, 1, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 1, 1, 0, 0 },
+				{ 0, 0, 0, 0, 1, 1, 1, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0, 0 }, };
+		System.out.println(countIslands(m1));
+
+		int[][] m2 = { { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+				{ 0, 1, 1, 1, 1, 1, 1, 1, 0 }, { 0, 1, 1, 1, 0, 0, 0, 1, 0 },
+				{ 0, 1, 1, 0, 0, 0, 1, 1, 0 }, { 0, 0, 0, 0, 0, 1, 1, 0, 0 },
+				{ 0, 0, 0, 0, 1, 1, 1, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0, 0 }, };
+		System.out.println(countIslands(m2));
+
+	}
+
+}
